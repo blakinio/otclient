05b8354df8556f542fa6f5acd17b872e2810acb9
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-26 00:48:20 +0200

    fix(entergame): add character list lifecycle path helper

---
+++ modules/client_entergame/characterlist_lifecycle_core.lua
@@ -0,0 +1,41 @@
+local CharacterListLifecycleCore = {}
+
+local MODULE_UI_ROOT = '/client_entergame/'
+local DEFAULT_LAYOUT = 'characterlist'
+
+local function isNonEmptyString(value)
+    return type(value) == 'string' and value ~= ''
+end
+
+function CharacterListLifecycleCore.normalizeLayoutPath(resources, layoutName)
+    if type(resources) ~= 'table' or type(resources.guessFilePath) ~= 'function' then
+        return nil, 'invalid_resources'
+    end
+
+    local requestedLayout = isNonEmptyString(layoutName) and layoutName or DEFAULT_LAYOUT
+    local layoutPath = resources.guessFilePath(requestedLayout, 'otui')
+    if not isNonEmptyString(layoutPath) then
+        return nil, 'invalid_layout'
+    end
+
+    layoutPath = layoutPath:gsub('\\', '/')
+    if layoutPath:sub(1, 1) == '/' then
+        return layoutPath
+    end
+
+    while layoutPath:sub(1, 2) == './' do
+        layoutPath = layoutPath:sub(3)
+    end
+
+    if layoutPath == '..' or layoutPath:sub(1, 3) == '../' or layoutPath:find('/../', 1, true) then
+        return nil, 'invalid_layout'
+    end
+
+    return MODULE_UI_ROOT .. layoutPath
+end
+
+function CharacterListLifecycleCore.shouldRecreate(isCreated, characters, account)
+    return not isCreated and type(characters) == 'table' and account ~= nil
+end
+
+return CharacterListLifecycleCore

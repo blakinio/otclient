b04c6d3dc69797268f03af9823db4a7c99e0ee38
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-27 12:20:51 +0200

    fix(assets): restack release selection on current main

---
+++ modules/client_assets/client_assets_release_adapter.lua
@@ -0,0 +1,38 @@
+ClientAssetsReleaseAdapter = ClientAssetsReleaseAdapter or {}
+
+local Selector = rawget(_G, 'ClientAssetsReleaseSelector') or dofile('client_assets_release_selector')
+local originalGetJSON = nil
+local wrappedGetJSON = nil
+
+local function serviceConfig()
+    return Services and Services.clientAssets
+end
+
+function ClientAssetsReleaseAdapter.init()
+    if wrappedGetJSON or not HTTP or type(HTTP.getJSON) ~= 'function' then
+        return
+    end
+
+    originalGetJSON = HTTP.getJSON
+    wrappedGetJSON = function(url, callback)
+        if not Selector.isConfiguredReleasesUrl(url, serviceConfig()) or type(callback) ~= 'function' then
+            return originalGetJSON(url, callback)
+        end
+
+        return originalGetJSON(url, function(data, err)
+            if not err and type(data) == 'table' then
+                data = Selector.prepareReleases(data)
+            end
+            return callback(data, err)
+        end)
+    end
+    HTTP.getJSON = wrappedGetJSON
+end
+
+function ClientAssetsReleaseAdapter.terminate()
+    if wrappedGetJSON and HTTP and HTTP.getJSON == wrappedGetJSON then
+        HTTP.getJSON = originalGetJSON
+    end
+    originalGetJSON = nil
+    wrappedGetJSON = nil
+end

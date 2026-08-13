b196c5019a682ebdeb4a4dfba7cf762a5757e0a2
blakinio <75369544+blakinio@users.noreply.github.com>
2026-08-13 18:42:54 +0200

    test(protocol): exercise full version Lua binding

--- tests/unit/protocol/game_login_version_string_test.cpp
+++ tests/unit/protocol/game_login_version_string_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <client/client.h>
 #include <client/gameconfig.h>
+#include <framework/luaengine/luainterface.h>
 
 TEST(GameLoginVersionString, FallsBackToNumericClientVersionWhenUnset)
 {
@@ -15,4 +17,19 @@ TEST(GameLoginVersionString, UsesConfiguredFullClientVersionForSerialization)
     config.setClientVersionString("15.32.df7b29");
 
     EXPECT_EQ(config.getGameLoginVersionString(1532), "15.32.df7b29");
+}
+
+TEST(GameLoginVersionString, LuaSingletonBindingSetsAndReadsFullVersion)
+{
+    g_lua.init();
+    Client::registerLuaFunctions();
+
+    EXPECT_NO_THROW(g_lua.runBuffer(R"lua(
+        g_gameConfig.setClientVersionString('15.32.df7b29')
+        assert(g_gameConfig.getClientVersionString() == '15.32.df7b29')
+    )lua", "game-login-version-binding-test"));
+    EXPECT_EQ(g_gameConfig.getClientVersionString(), "15.32.df7b29");
+
+    g_gameConfig.setClientVersionString({});
+    g_lua.terminate();
 }
